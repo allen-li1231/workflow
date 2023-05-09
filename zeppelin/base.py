58b6257e504c5dd65efee733af2247c20513e7fe
allen-li1231 <supermrli@hotmail.com>
2023-05-09 11:30:20 +0800

    rearrange some basic methods in  base

--- zeppelin/base.py
+++ zeppelin/base.py
@@ -4,8 +4,8 @@ import logging
 import requests
 
 from .. import logger
-from ..decorators import retry
-from ..settings import ZEPPELIN_URL, ZEPPELIN_PARAGRAPH_CONFIG, PROGRESSBAR
+from ..decorators import ensure_login, retry
+from ..settings import ZEPPELIN_URL, ZEPPELIN_PARAGRAPH_CONFIG, ZEPPELIN_INACTIVE_TIME, PROGRESSBAR
 
 
 class ZeppelinBase(requests.Session):
@@ -22,42 +22,82 @@ class ZeppelinBase(requests.Session):
         self.headers["User-Agent"] = \
             "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
 
+    @property
+    def is_logged_in(self) -> bool:
+        if not hasattr(self, "_last_execute"):
+            return False
+
+        return time.perf_counter() - self._last_execute < ZEPPELIN_INACTIVE_TIME
+
+    def login(self, username: str = None, password: str = None):
+        self.username = username or self.username
+        self._password = password or self._password
+        if self.username is None and self._password is None:
+            raise ValueError("please provide username and password")
+
+        if self.username is None and self._password is not None:
+            raise KeyError("username must be specified with password")
+
+        if self.username is not None and self._password is None:
+            print("Please provide Zeppelin password:", end='')
+            self._password = input("")
+
+        self.log.debug(f"logging in for user: [{self.username}]")
+        res = self._login()
+        if res.status_code != 200:
+            self._password = None
+            self.log.error('login failed for [%s] at %s'
+                           % (self.username, self.base_url))
+            raise ValueError('login failed for [%s] at %s'
+                             % (self.username, self.base_url))
+        else:
+            self.log.info('login succeeful [%s] at %s'
+                          % (self.username, self.base_url))
+
+        return self
+
     @retry(__name__)
     def _login(self, username, password):
         url = self.base_url + "/api/login"
         res = self.post(url, data={"userName": username, "password": password})
         return res
 
+    @ensure_login
     @retry(__name__)
     def _list_notes(self):
         url = self.base_url + "/api/notebook"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _create_note(self, name: str, paragraphs: list):
         url = self.base_url + "/api/notebook"
         res = self.post(url, json={"name": name, "paragraphs": paragraphs})
         return res
 
+    @ensure_login
     @retry(__name__)
     def _delete_note(self, note_id):
         url = self.base_url + f"/api/notebook/{note_id}"
         res = self.delete(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _import_note(self, note_json):
         url = self.base_url + f"/api/notebook/{self.note_id}"
         res = self.post(url, json=note_json)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _clone_note(self, note_id, name):
         url = self.base_url + f"/api/notebook/{note_id}"
         res = self.post(url, json={"name": name})
         return res
 
+    @ensure_login
     @retry(__name__)
     def _export_note(self, note_id):
         url = self.base_url + f"/api/notebook/export/{note_id}"
@@ -84,54 +124,79 @@ class NoteBase(requests.Session):
         self.headers = zeppelin.headers
         self.cookies = zeppelin.cookies
 
+        self.zeppelin = zeppelin
+        # register method
+        self.login = zeppelin.login
+
+    @property
+    def _last_execute(self):
+        return self.zeppelin._last_execute
+
+    @_last_execute.setter
+    def _last_execute(self, value):
+        self.zeppelin._last_execute = value
+
+    @property
+    def is_logged_in(self):
+        return self.zeppelin.is_logged_in
+    
+    @ensure_login
     @retry(__name__)
     def _get_all_status(self):
         url = self.base_url + f"/api/notebook/job/{self.note_id}"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _get_info(self):
         url = self.base_url + f"/api/notebook/{self.note_id}"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _delete_note(self):
         url = self.base_url + f"/api/notebook/{self.note_id}"
         res = self.delete(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _clone_note(self, name):
         url = self.base_url + f"/api/notebook/{self.note_id}"
         res = self.post(url, json={"name": name})
         return res
 
+    @ensure_login
     @retry(__name__)
     def _export_note(self):
         url = self.base_url + f"/api/notebook/export/{self.note_id}"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _run_all(self):
         url = self.base_url + f"/api/notebook/job/{self.note_id}"
         res = self.post(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _stop_all(self, name):
         url = self.base_url + f"/api/notebook/job/{self.note_id}"
         res = self.delete(url, json={"name": name})
         return res
 
+    @ensure_login
     @retry(__name__)
     def _clear_all_result(self):
         url = self.base_url + f"/api/notebook/{self.note_id}/clear"
         res = self.put(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _create_paragraph(self, text: str, title=None, index: int = -1, config: dict = None):
         url = self.base_url + f"/api/notebook/{self.note_id}/paragraph"
@@ -145,30 +210,35 @@ class NoteBase(requests.Session):
         res = self.post(url, json=payload)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _add_cron(self, cron: str, release_resource=False):
         url = self.base_url + f"/api/notebook/cron/{self.note_id}"
         res = self.post(url, json={"cron": cron, "releaseResource": release_resource})
         return res
 
+    @ensure_login
     @retry(__name__)
     def _remove_cron(self):
         url = self.base_url + f"/api/notebook/cron/{self.note_id}"
         res = self.delete(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _get_cron(self):
         url = self.base_url + f"/api/notebook/cron/{self.note_id}"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _get_permission(self):
         url = self.base_url + f"/api/notebook/{self.note_id}/permissions"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _set_permission(self, readers: list, owners: list, runners: list, writers: list):
         url = self.base_url + f"/api/notebook/cron/{self.note_id}"
@@ -186,7 +256,7 @@ class ParagraphBase(requests.Session):
     def __init__(self, note: NoteBase, paragraph_id: str):
         super(ParagraphBase, self).__init__()
 
-        self.paragraph_id = paragraph_id
+        self._paragraph_id = paragraph_id
         self.note_id = note.note_id
         self.base_url = note.base_url
         self.log = logging.getLogger(__name__ + f".ParagraphBase")
@@ -198,18 +268,38 @@ class ParagraphBase(requests.Session):
         self.headers = note.headers
         self.cookies = note.cookies
 
+        self.zeppelin = note.zeppelin
+        self.note = note
+        # register method
+        self.login = note.zeppelin.login
+
+    @property
+    def _last_execute(self):
+        return self.zeppelin._last_execute
+
+    @_last_execute.setter
+    def _last_execute(self, value):
+        self.zeppelin._last_execute = value
+
+    @property
+    def is_logged_in(self):
+        return self.zeppelin.is_logged_in
+
+    @ensure_login
     @retry(__name__)
     def _get_info(self):
         url = self.base_url + f"/api/notebook/{self.note_id}/paragraph/{self.paragraph_id}"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _get_status(self):
         url = self.base_url + f"/api/notebook/job/{self.note_id}/{self.paragraph_id}"
         res = self.get(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _update_text(self, text: str, title=None):
         url = self.base_url + f"/api/notebook/{self.note_id}/paragraph/{self.paragraph_id}"
@@ -220,12 +310,21 @@ class ParagraphBase(requests.Session):
         res = self.put(url, json=payload)
         return res
 
+    @ensure_login
+    @retry(__name__)
+    def _update_config(self, config):
+        url = self.base_url + f"/api/notebook/{self.note_id}/paragraph/{self.paragraph_id}/config"
+        res = self.put(url, json=config)
+        return res
+
+    @ensure_login
     @retry(__name__)
     def _drop(self):
-        url = self.base_url + f"/api/notebook/{self.note_id}/paragraph/{self.paragraph_id}/config"
+        url = self.base_url + f"/api/notebook/{self.note_id}/paragraph/{self.paragraph_id}"
         res = self.delete(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _execute(self, sync=True, option: dict = None):
         if sync:
@@ -236,12 +335,14 @@ class ParagraphBase(requests.Session):
         res = self.post(url, json=option)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _stop(self):
         url = self.base_url + f"/api/notebook/job/{self.note_id}/{self.paragraph_id}"
         res = self.delete(url)
         return res
 
+    @ensure_login
     @retry(__name__)
     def _move_to_index(self, index: int):
         url = self.base_url + f"/api/notebook/{self.note_id}/paragraph/{self.paragraph_id}/move/{index}"
