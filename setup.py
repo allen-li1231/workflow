4fc51d1d0c3ed4ee6fbf56c15ba49a0afb5e43e7
allen-li1231 <supermrli@hotmail.com>
2023-10-23 17:56:56 +0800

    move setup.py to outside src directory to make inside code clean

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="risk_workflow", #Name
-    version="0.1.0", #Version
-    packages = find_packages()  # Automatically find the packages that are recognized in the '__init__.py'.
-)
