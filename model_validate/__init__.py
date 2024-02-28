b26d15ec89e5bcc86040b3fdad95c1653a301cbd
allen-li1231 <supermrli@hotmail.com>
2024-02-28 17:59:51 +0800

    make workflow ready to distribute

--- model_validate/__init__.py
+++
@@ -1,4 +0,0 @@
-from .reports import model_report, binary_classification_report, binary_classification_report_by_date
-
-__all__ = [model_report, binary_classification_report,
-           binary_classification_report_by_date]
